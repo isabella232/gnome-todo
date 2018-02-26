fd5e0fb52fb3b21f6c785b0f9bbfbfd1b8569538
Piotr Drąg <piotrdrag@gmail.com>
2018-02-26 18:43:17 +0100

    todoist: Use a Unicode apostrophe in a translatable string
    See https://developer.gnome.org/hig/stable/typography.html

--- plugins/todoist/gtd-provider-todoist.c
+++ plugins/todoist/gtd-provider-todoist.c
@@ -568,7 +568,7 @@ check_post_response_for_errors (RestProxyCall     *call,
               g_set_error (error,
                            GTD_PROVIDER_TODOIST_ERROR,
                            GTD_PROVIDER_TODOIST_ERROR_NOT_ALLOWED,
-                           _("GNOME To Do doesn't have the necessary permissions to perform this action: %s"),
+                           _("GNOME To Do doesn’t have the necessary permissions to perform this action: %s"),
                            message);
               break;
 
