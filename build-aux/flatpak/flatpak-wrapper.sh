2c45bd0f33ee3297b36790cfba15b47d6d231b09
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2019-02-08 20:20:11 -0200

    flatpak: Wait 4 seconds before executing gnome-todo
    There might be a race condition when running GNOME To Do
    right after spawning the daemons, so give them a bit more
    time to load.

--- build-aux/flatpak/flatpak-wrapper.sh
+++ build-aux/flatpak/flatpak-wrapper.sh
@@ -8,8 +8,8 @@ fi
 export GIO_USE_NETWORK_MONITOR=base
 gsettings reset org.gnome.evolution-data-server network-monitor-gio-name
 /app/libexec/evolution-source-registry &
-sleep 1
+sleep 2
 /app/libexec/evolution-addressbook-factory -r &
 /app/libexec/evolution-calendar-factory -r &
-sleep 1
+sleep 2
 /app/bin/gnome-todo "$@"
