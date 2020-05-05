47282328c114f29d7ec4a66a8425a87229f43723
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2020-05-05 09:33:36 -0300

    manager: Add inbox model

--- src/engine/gtd-manager.h
+++ src/engine/gtd-manager.h
@@ -79,4 +79,6 @@ GListModel*          gtd_manager_get_all_tasks_model             (GtdManager    
 
 GListModel*          gtd_manager_get_tasks_model                 (GtdManager         *self);
 
+GListModel*          gtd_manager_get_inbox_model                 (GtdManager         *self);
+
 G_END_DECLS
